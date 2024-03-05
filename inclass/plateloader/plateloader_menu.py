397f59cdd144fefb8004db5b542d887f9cb05dca
David Fisher <fisherds@rose-hulman.edu>
2024-03-05 10:17:43 -0500

    python menu

---
+++ inclass/plateloader/plateloader_menu.py
@@ -0,0 +1,25 @@
+def main():
+    print("Plate Loader")
+
+    ser = None  # TODO: Open a serial object.
+
+    while True:
+        print("0: Exit")
+        print("1: RESET")
+        print("2: X-AXIS")
+
+        choice = input("Make a selection: ")
+        if choice == "0":
+            break
+        elif choice == "1":
+            send_message(ser, "RESET")
+        elif choice == "2":
+            to_pos = input("Where to? (1-5): ")
+            send_message(ser, f"X-AXIS {to_pos}")
+        else:
+            print("Unknown command.  Please try again.")
+
+def send_message(ser, command):
+    print(f"TODO, send command {command} to the serial object.")
+
+main()
