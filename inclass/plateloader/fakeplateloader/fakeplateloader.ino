268564056c807ae2b8c9789d73951b9f3879ec55
David Fisher <fisherds@rose-hulman.edu>
2024-03-05 09:13:16 -0500

    plateloader basics

--- inclass/plateloader/fakeplateloader/fakeplateloader.ino
+++ inclass/plateloader/fakeplateloader/fakeplateloader.ino
@@ -1,10 +1,36 @@
+String inputString = "";
+bool isStringComplete = false;
+
 void setup() {
-    pinMode(13, OUTPUT);
+    inputString.reserve(200);
+    Serial.begin(19200);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+    if (isStringComplete) {
+        // Do stuff with it!
+        if (inputString.equals("RESET")) {
+            Serial.println("READY, SAGIAN PE Loader, ROM...");
+        } if (inputString.startsWith("X-AXIS ")) {
+            Serial.println("READY");
+        } else {
+            Serial.println("Unknown command!");
+            Serial.println(inputString);
+        }
+
+
+        inputString = "";
+        isStringComplete = false;
+    }
+}
+
+void serialEvent() {
+    while (Serial.available()) {
+        char inChar = (char) Serial.read();
+        if (inChar == '\n') {
+            isStringComplete = true;
+        } else {
+            inputString += inChar;
+        }
+    }
 }
