e64e41e7691068c9ccb80a17862378735ecdaea0
David Fisher <fisherds@rose-hulman.edu>
2024-04-12 20:31:22 -0400

    tank test

---
+++ prep/Python/tank/quick_motor_test.py
@@ -0,0 +1,10 @@
+Motor_A_EN = 4
+Motor_B_EN = 17
+
+Motor_A_Pin1 = 14
+Motor_A_Pin2 = 15
+Motor_B_Pin1 = 27
+Motor_B_Pin2 = 18
+left_motor = Motor(Motor_B_EN, Motor_B_Pin1, Motor_B_Pin2)
+right_motor = Motor(Motor_A_EN, Motor_A_Pin2, Motor_A_Pin1)
+
